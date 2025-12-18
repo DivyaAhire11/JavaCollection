34efdc94bdb82370a14c7dc881fbfe5da2c3a1b5
DivyaAhire11 <divyaahire121@gmail.com>
2025-12-18 12:35:04 +0530

    Collection

---
+++ Demo.java
@@ -0,0 +1,21 @@
+//Collection is a group of anything but objects only
+//Similar data(Homogeneous data) ,Dissimilar data(Heterogeneous data),unique,duplicate
+//unlimited Sized
+
+//primitive
+// int a = 5;
+//int a[] = new int[10];  //collection or group of primitives
+
+// non-primitive
+// Student st = new Student();
+// Student st[] = new Student[10] ; //Collection or group of non-primitives i.e Objects
+
+/*
+   Limitation of array:
+     1. Homogeneous elements
+     2.Fixed Sized
+     3.Static 
+     4.Automatically grow and Shrink not possible
+     5.Memory Wastage
+
+*/
