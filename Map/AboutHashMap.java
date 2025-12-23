8bdc0b3a99a6fb5c231275f3b93692344eb157a2
DivyaAhire11 <divyaahire121@gmail.com>
2025-12-23 21:58:09 +0530

    Map Methods

--- Map/AboutHashMap.java
+++ Map/AboutHashMap.java
@@ -1,9 +1,12 @@
 package Map;
 
 import java.util.HashMap;
+import java.util.Set;
+import java.util.Map;
 
 public class AboutHashMap {
     public static void main(String[] args) {
+
         HashMap<Integer, String> hm = new HashMap<>();
 
         // ----------------Adding Elements(put)-----------------------
@@ -27,15 +30,51 @@ public class AboutHashMap {
         System.out.println("Check if Value Present in map? :" + hm.containsValue("Shubham")); // true
         System.out.println(hm.containsValue("Ritudi")); // false
 
-        System.out.println("Is Empty? : "+hm.isEmpty());  //false
-        System.out.println("Size of the map : "+hm.size());  //4
+        System.out.println("Is Empty? : " + hm.isEmpty()); // false
+        System.out.println("Size of the map : " + hm.size()); // 4
+
+        hm.put(null, null);
+        hm.put(3, null);
+        hm.put(7, "7");
+
+        System.out.println(hm); // {null=null, 2=Akshit, 3=null, 53=Shubham, 7=7, 73=Neha, 111=Ritu}
+
+        hm.remove(null); // key
+        hm.remove(7, "7");
+
+        System.out.println(hm); // {2=Akshit, 3=null, 53=Shubham, 73=Neha, 111=Ritu}
+
+        // -----------------keySet()---------------------
+        // Set<Integer> keys = hm.keySet();
+        // for(int i : keys){
+        // System.out.println(hm.get(i));
+        // }
+
+        Set<Integer> keys = hm.keySet();
+        System.out.println(keys); // [2, 3, 53, 73, 111]
+
+        for (int x : hm.keySet()) {
+            System.out.println(hm.get(x));
+        }
+
+        hm.replace(3, "Sayali");
+        System.out.println(hm); // {2=Akshit, 3=Sayali, 53=Shubham, 73=Neha, 111=Ritu}
+
+        // Set of collection of entries
+
+        Set<Map.Entry<Integer, String>> entry = hm.entrySet();
+        System.out.println(entry);// [2=Akshit, 3=Sayali, 53=Shubham, 73=Neha, 111=Ritu]
+
+        for(Map.Entry<Integer,String> i : entry){
+            System.out.println(i.getKey() +" : "+i.getValue());
+            i.setValue(i.getValue().toUpperCase());
+           
+        }
+
+          System.out.println(entry);  //[2=AKSHIT, 3=SAYALI, 53=SHUBHAM, 73=NEHA, 111=RITU]
+          System.out.println(hm);   //{2=AKSHIT, 3=SAYALI, 53=SHUBHAM, 73=NEHA, 111=RITU}
 
 
-        hm.put(null,null);
-        hm.put(3,null);
-        hm.put(7,"7");
-
-        System.out.println(hm); //{null=null, 2=Akshit, 3=null, 53=Shubham, 7=7, 73=Neha, 111=Ritu}
-
+          
     }
 }
